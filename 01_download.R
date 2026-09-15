1de42ac7a96b48dd9515816d86b83969e2cfa3f2
Behnaz Abedi <Behnaz Abedi>
2026-09-16 00:12:32 +0430

    Clean download and QC scripts

--- 01_download.R
+++ 01_download.R
@@ -3,7 +3,7 @@ library(curl)
 dir.create("data/raw_fastq", recursive = TRUE, showWarnings = FALSE)
 
 download_one <- function(url, destfile) {
-  cat("شروع دانلود:", destfile, "\n")
+  cat("Downloading:", destfile, "\n")
   result <- multi_download(
     urls = url,
     destfiles = destfile,
@@ -13,3 +13,22 @@ download_one <- function(url, destfile) {
   print(result[, c("success", "status_code", "error")])
 }
 
+download_one(
+  "https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR285/013/SRR28532213/SRR28532213_1.fastq.gz",
+  "data/raw_fastq/normal_1.fastq.gz"
+)
+
+download_one(
+  "https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR285/013/SRR28532213/SRR28532213_2.fastq.gz",
+  "data/raw_fastq/normal_2.fastq.gz"
+)
+
+download_one(
+  "https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR285/016/SRR28532216/SRR28532216_1.fastq.gz",
+  "data/raw_fastq/tumor_1.fastq.gz"
+)
+
+download_one(
+  "https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR285/016/SRR28532216/SRR28532216_2.fastq.gz",
+  "data/raw_fastq/tumor_2.fastq.gz"
+)
--- 02_qc.R
+++ 02_qc.R
@@ -5,25 +5,9 @@ dir.create("data/trimmed_fastq", recursive = TRUE, showWarnings = FALSE)
 rfastp(
   read1 = "data/raw_fastq/normal_1.fastq.gz",
   read2 = "data/raw_fastq/normal_2.fastq.gz",
-  outputFastq = "data/trimmed_fastq/normal"
-)
-
-rfastp(
-  read1 = "data/raw_fastq/tumor_1.fastq.gz",
-  read2 = "data/raw_fastq/tumor_2.fastq.gz",
-  outputFastq = "data/trimmed_fastq/tumor"
-)
-library(Rfastp)
-
-dir.create("data/trimmed_fastq", showWarnings = FALSE, recursive = TRUE)
-
-rfastp(
-  read1 = "data/raw_fastq/normal_1.fastq.gz",
-  read2 = "data/raw_fastq/normal_2.fastq.gz",
   outputFastq = "data/trimmed_fastq/normal_trimmed.fastq.gz",
   thread = 4
 )
-
 
 rfastp(
   read1 = "data/raw_fastq/tumor_1.fastq.gz",
